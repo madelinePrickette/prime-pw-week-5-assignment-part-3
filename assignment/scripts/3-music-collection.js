c20b846811742b1c1c3e29f6d12dff8cc5209df4
Madeline Prickette <madelineprickette@gmail.com>
2022-07-09 15:04:45 -0500

    greated function containing object, empty array, and added 6 albums, console.logged array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let record = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(record);
+    return record;
+}
+
+console.log(addToCollection('Love Scene', 'Kiabits', 2022));
+console.log(addToCollection(`Sor's Tiles`, 'Rien', 2022));
+console.log(addToCollection('Nature & Me', '7&Nine', 2021));
+console.log(addToCollection('Mahina', 'Natahsa Ghosh', 2021));
+console.log(addToCollection('Poison Tree', 'Grouper', 2008));
+console.log(addToCollection('Alien Observer', 'Grouper', 2011));
+
+console.log(collection);
